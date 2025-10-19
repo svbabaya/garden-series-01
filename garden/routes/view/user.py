27bd21c1112e1c71e7213c91e23b63654da3c5c9
Serge Babayan <babayan.serge@gmail.com>
2025-10-20 01:27:06 +0300

    Add html templates for menu list

--- garden/routes/view/user.py
+++ garden/routes/view/user.py
@@ -6,22 +6,47 @@ router = APIRouter()
 
 # @router.get("/plants")
 # async def get_all_plants():
-#     return [{"template": "selection.html"}]
 #
 #
 # @router.get("/{category_id}")
 # async def get_category(category_id: int):
-#     return [{"template": "category.html"}]
 #
 #
 # @router.get("/{category_id}/{plant_id}")
 # async def get_plant(category_id: int, plant_id: int):
-#     return [{"template": "plant.html"}]
 
 
 @router.get("/history", name="history")
 async def history(request: Request):
     return templates.TemplateResponse(
         name="user/history.html",
+        context={"request": request}
+    )
+
+@router.get("/search", name="search")
+async def about(request: Request):
+    return templates.TemplateResponse(
+        name="user/search.html",
+        context={"request": request}
+    )
+
+@router.get("/art", name="art")
+async def about(request: Request):
+    return templates.TemplateResponse(
+        name="user/art.html",
+        context={"request": request}
+    )
+
+@router.get("/ideas", name="ideas")
+async def about(request: Request):
+    return templates.TemplateResponse(
+        name="user/ideas.html",
+        context={"request": request}
+    )
+
+@router.get("/details", name="details")
+async def about(request: Request):
+    return templates.TemplateResponse(
+        name="user/details.html",
         context={"request": request}
     )
